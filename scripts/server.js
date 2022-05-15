7eefe5e0579d00f69c79138442b765c2b383435e
Srijan Verma <verma.srijan19@gmail.com>
2022-05-15 16:51:46 +0530

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -10,18 +10,28 @@ app.use(express.urlencoded({ extended: true }));
 let wl= null
 async function start()
 {
- wl = (await hre.ethers.getContractAt("WLToken", "0xDcCA35364b673Ab1fB13Bb3cbFcbe9fB19C6A8F9"));
+    wl = (await hre.ethers.getContractAt("WLToken", "0xDcCA35364b673Ab1fB13Bb3cbFcbe9fB19C6A8F9"));
 
 }
 
 start()
 async function mint(address,count) {
-     // wl = (await hre.ethers.getContractAt("WLToken", "0xDcCA35364b673Ab1fB13Bb3cbFcbe9fB19C6A8F9"));
-    await wl.mint(address, count);
+    // wl = (await hre.ethers.getContractAt("WLToken", "0xDcCA35364b673Ab1fB13Bb3cbFcbe9fB19C6A8F9"));
+    try {
+        await wl.mint(address, count);
+    }
+    catch (e) {
+        console.log(e);
+    }
 }
 async function burn() {
-     // wl = (await hre.ethers.getContractAt("WLToken", "0xDcCA35364b673Ab1fB13Bb3cbFcbe9fB19C6A8F9"));
-    await wl.burn2("0x22c8F656a4599A79619E5756A5b6cB8eaA193bD7", 10);
+    // wl = (await hre.ethers.getContractAt("WLToken", "0xDcCA35364b673Ab1fB13Bb3cbFcbe9fB19C6A8F9"));
+    try {
+        await wl.burn2("0x22c8F656a4599A79619E5756A5b6cB8eaA193bD7", 10);
+    }
+    catch  {
+        console.log(e);
+    }
 }
 async function balanceOf(address) {
     // wl = (await hre.ethers.getContractAt("WLToken", "0xDcCA35364b673Ab1fB13Bb3cbFcbe9fB19C6A8F9"));
@@ -29,21 +39,40 @@ async function balanceOf(address) {
 }
 
 
-app.post('/mint', (req, res) => {
+app.get('/mint', (req, res) => {
 
-    mint(req.body.address,req.body.count);
-    res.send('Minting...')
+    try {
+        mint(req.query.add, req.query.count);
+        res.send('Minting...')
+    }
+    catch (e) {
+        console.log(e)
+    }
 })
-app.post('/burn', (req, res) => {
-    burn(req.body.address,req.body.count);
-    res.send('Burning...')
+app.get('/burn', (req, res) => {
+    try {
+        burn(req.query.add, req.query.count);
+        res.send('Burning...')
+    }
+    catch (e) {
+        console.log(e)
+    }
 })
 app.get('/balance', (req, res) => {
-    console.log(req.query.add);
-    balanceOf(req.query.add).then(function(result){
-        res.send(result.toString())
-    })
+    try {
+        console.log(req.query.add);
+        balanceOf(req.query.add).then(function (result) {
+            res.send(result.toString())
+        })
+    }
+    catch (e) {
+        console.log(e)
+    }
 })
 
-
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+try {
+    app.listen(process.env.PORT ||port, () => console.log(`app listening on port ${port}!`))
+}
+catch (e) {
+    console.log(e)
+}
